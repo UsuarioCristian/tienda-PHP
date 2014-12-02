2ca30cbc22aa6f339c68f811d7b682503ba5bbb2
federico <federicofernandez24@gmail.com>
2014-12-02 01:05:21 -0200

    Primera vista de producto.

---
+++ views/product/viewProduct.php
@@ -0,0 +1,18 @@
+<div class="panel panel-default">
+  <div class="panel-body">
+    <img src="/public/images/products/iMac.png" alt="iMac" class="img-thumbnail">
+		<div class="thumbnail">
+		<img class="img-responsive" src="application/public/images/products/iMac.png" alt="">
+		<div class="caption-full">
+		<h4 class="pull-right">$24.99</h4>
+		<h4><a href="#">Product Name</a>
+		</h4>
+		<p>See more snippets like these online store reviews at <a target="_blank" href="http://bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+		<p>Want to make these reviews work? Check out
+		<strong><a href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">this building a review system tutorial</a>
+		</strong>over at maxoffsky.com!</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+		</div>
+		</div>
+  </div>
+</div>
